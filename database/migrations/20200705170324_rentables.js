7f1c5c9e8ed30a3d34101a64efbf55445062afc1
Landon Turner <lturner@teamworks.com>
2020-07-05 17:56:26 -0400

    add url fields ftorentables table

--- database/migrations/20200705170324_rentables.js
+++ database/migrations/20200705170324_rentables.js
@@ -4,6 +4,8 @@ exports.up = function(knex) {
     tbl.increments('id').primary();
     tbl.string('itemName', 64).unique().notNullable();
     tbl.text('description', 500);
+    tbl.string('designUrl', 255);
+    tbl.string('thumbnailUrl', 255);
     tbl.boolean('isAvailable').defaultTo(true);
     tbl.integer('departmentId', 9)
     .unsigned()
