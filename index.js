6cd55792ffc07869527eed9a97a3d01c34c8b152
Landon Turner <lturner@teamworks.com>
2020-07-05 15:24:38 -0400

    server running

---
+++ index.js
@@ -0,0 +1,9 @@
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 4000;
+
+server.listen( 
+    PORT, () => {
+        console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    }
+)
