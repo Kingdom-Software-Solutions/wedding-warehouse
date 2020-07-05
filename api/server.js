661085451b89b1a4ad86e6115d5b82c67a4bfdd4
Landon Turner <lturner@teamworks.com>
2020-07-05 15:52:24 -0400

    api initialized

--- api/server.js
+++ api/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-const server = express(); 
+const environment = process.env.NODE_ENV
+
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.json(`STATUS: Wedding Warehouse api is running in ${environment} mode`);
+  });
 
 module.exports = server;
