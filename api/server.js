523c62a398601a13763641e3f62a77259bdbc088
Landon Turner <lturner@teamworks.com>
2020-07-06 23:04:23 -0400

    fix bcrypt install, call helmet and cors

--- api/server.js
+++ api/server.js
@@ -4,8 +4,16 @@ const cors = require('cors');
 
 const environment = process.env.NODE_ENV
 
+// ROUTER IMPORTS
+const authRouter = require('../operations/auth/authRouter');
+
 const server = express();
+server.use(helmet());
 server.use(express.json());
+server.use(cors());
+
+// USE ROUTERS
+server.use('/api/auth', authRouter)
 
 server.get("/", (req, res) => {
     res.json(`STATUS: Wedding Warehouse api is running in ${environment} mode`);
