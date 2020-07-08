bdcb810028061fe6bc463f8920266fdc6a2df726
Landon Turner <lturner@teamworks.com>
2020-07-08 00:49:03 -0400

    add comment to users

--- routers/users/userRouter.js
+++ routers/users/userRouter.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
 const Models = require("../helpers/models");
 
+// need to at least add a put before adding ui and hosting for mel to add inventory so I can flip her user to admin
+
 module.exports = router;
