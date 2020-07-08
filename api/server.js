fc2c302cf89ebe149c73275f23a4e6366c2695fb
Landon Turner <lturner@teamworks.com>
2020-07-08 00:11:20 -0400

    create and get departments

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const morgan = require('morgan')
 
 const environment = process.env.NODE_ENV
 
@@ -11,6 +12,19 @@ const deptRouter = require('../routers/departments/deptRouter')
 const server = express();
 server.use(helmet());
 server.use(express.json());
+server.use(
+    morgan(function (tokens, req, res) {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, "content-length"),
+            "-",
+            tokens["response-time"](req, res),
+            "ms",
+        ].join(" ");
+    })
+)
 server.use(cors());
 
 // USE ROUTERS
--- routers/departments/deptRouter.js
+++ routers/departments/deptRouter.js
@@ -1,18 +1,31 @@
 const router = require("express").Router();
-const Models = require("../helpers/models");
-const heimdal = require("../auth/authenticator")
+// const Depts = require("../helpers/deptModel");
+const Models = require("../helpers/models")
+const heimdal = require("../auth/authenticator");
 
 // add a department
 router.post("/", (req, res) => {
     let dept = req.body;
 
     Models.Department.insert(dept)
-    .then(res =>{
+    .then(dept =>{
         res.status(201).json({message:"Department successfully added!", dept})
     })
     .catch(err => {
-        res.status(500).json({error: "err", errorMessage: "Oof, something went wrong on our end"})
-    })
+        console.log(err)
+        res.status(500).json({error: err, errorMessage: "Oof, something went wrong on our end"})
+    });
 })
+
+// find all depts
+router.get("/", (req, res) => {
+    Models.Department.find()
+    .then(depts => {
+        res.status(200).json(depts)
+    })
+    .catch(err => {
+        res.status(500).json({error: err, errorMessage: "Oof! Something went wrong on our end"})
+    });
+});
 
 module.exports = router;
