04b16d1ee4af8c4ce29d160eda8bed9444cf8050
Landon Turner <lturner@teamworks.com>
2020-07-08 21:51:49 -0400

    add find user by username

--- routers/users/userRouter.js
+++ routers/users/userRouter.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const Models = require("../helpers/models");
 
+router.get("/username", (req, res) => {
+    const username = req.body;
+    Models.Users.findBy(username)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({error: err, errorMessage: "Oof! Something went wrong on our end"})
+    })
+})
 router.put("/:id", (req, res) => {
     const { id } = req.params;
     const changes = req.body;
