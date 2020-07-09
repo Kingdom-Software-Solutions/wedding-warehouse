e70b82563cbe7e7276631faa0dcdc7e430b41fe9
Landon Turner <lturner@teamworks.com>
2020-07-08 20:51:08 -0400

    add inventory post

--- routers/inventory/inventoryRouter.js
+++ routers/inventory/inventoryRouter.js
@@ -1,4 +1,28 @@
 const router = require("express").Router();
 const Models = require("../helpers/models");
 
+// add an item to inventory
+router.post("/", (req, res) => {
+    let item = req.body;
+    if(item.departmentId) {
+        Models.Inventory.insert(item)
+        .then(newItem => {
+            res.status(201).json({message: "Item successfully added to DB!", newItem})
+        })
+        .catch(err => {
+            res.status(500).json({error: err, errorMessage: "Oof! Something went wrong on our end"})
+        })
+    } else {
+        res.status(400).json({error: "Missing departmentId"})
+    }
+})
+// get all inventory
+
+// get item by id
+
+// update an item by id
+
+// delete an item by id
+
+
 module.exports = router;
