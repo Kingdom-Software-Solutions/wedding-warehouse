96beb122301cd0e8c5b2c46d05fe126c0dbdb476
Landon Turner <lturner@teamworks.com>
2020-07-14 00:10:18 -0400

    add state to better scaffold form

--- ui-react/src/components/Inventory/AddInventory.js
+++ ui-react/src/components/Inventory/AddInventory.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addDept, addItem } from '../../redux/actions/warehouseActions'
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 import Button from '@material-ui/core/Button';
 import { AuthBtn } from '../material-ui/AuthBtn';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
 import { axiosWithEnv } from '../../utils/axiosWithEnv';
 
 const AddInventory = props => {
+    const dispatch = useDispatch()
     const [depts, setDepts] = useState([]);
     const [newItem, setNewItem] = useState({
         itemName: "",
         description: "",
         rentalRate: 0,
         buyNow: 0,
+        departmentId: NaN
     });
-    const [error, setError] = useState({ message: ""});
+    // used to show add dept field or not
+    const [toggleDept, setToggleDept] = useState(false);
 
     // get dept for <select> on mount
     useEffect(()=>{
+        // maybe convert to get depts redux dispatch
         axiosWithEnv().get("/api/departments")
         .then(res => {
             console.log(res)
@@ -29,28 +32,34 @@ const AddInventory = props => {
         })
     },[]);
 
+    const handleChanges = e =>{
+        setNewItem({
+            ...newItem,
+            [e.target.name]: e.target.value
+        });
+    };
+
 
     return (
         <div>
             <h3>Add Item to Inventory</h3>
             <form>
                 <label>Item Name</label>
-                <input />
+                <input name="itemName" onChange={handleChanges}/>
                 <label>Description</label>
-                <textarea name="description" />
+                <textarea name="description" onChange={handleChanges}/>
                 {/* image uploader here */}
                 <Button
                 variant="contained"
                 color="primary"
-                // className={classes.button}
                 startIcon={<AddAPhotoIcon />}
                 >
                 Upload
                 </Button>
                 <label>Rental Rate</label>
-                <input />
+                <input name="rentalRate" onChange={handleChanges}/>
                 <label>Buy Now Price</label>
-                <input />
+                <input name="buyNow" onChange={handleChanges}/>
                 <label htmlFor="deptId">Select Department</label>
                 <select name="deptId" required={true}>
                     <option value="" disabled selected>Required</option>
@@ -65,7 +74,19 @@ const AddInventory = props => {
                             );
                     })};
                 </select>
-                <AuthBtn>Submit</AuthBtn>
+                { toggleDept ? <>
+                <input name="name" />
+                <Button >Add</Button>
+                <Button onClick={()=>{
+                    setToggleDept(false)
+                }}>Cancel</Button>
+                </>
+                :
+                <Button onClick={()=>{
+                    setToggleDept(true)
+                }}>Add New Department</Button>
+                }
+                <AuthBtn type="submit">Submit</AuthBtn>
                 <Button href="/inventory">
                     Back
                 </Button>
@@ -74,5 +95,10 @@ const AddInventory = props => {
     )
 };
 
+// const mapStateToProps = state => {
+//     return {
+//         isPosting: state.warehouseReducer.isPosting
+//     }
+// }
 
 export default AddInventory;
