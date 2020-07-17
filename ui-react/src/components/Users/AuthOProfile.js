70728493080701a4219021557fc8175e7aec721b
Landon Turner <lturner@teamworks.com>
2020-07-17 19:19:24 -0400

    add get user metadata

--- ui-react/src/components/Users/AuthOProfile.js
+++ ui-react/src/components/Users/AuthOProfile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
@@ -6,6 +6,35 @@ const Profile = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [userMetadata, setUserMetadata] = useState(null);
   console.log(user)
+  useEffect(() => {
+      // FETCHES USER META DATA, BREAK THIS FILE OUT INTO SOMETHING
+    const getUserMetadata = async () => {
+      const domain = "mels-warehouse-auth";
+  
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}`,
+          scope: "read:current_user",
+        });
+  
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+  
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+  
+        const { user_metadata } = await metadataResponse.json();
+  
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+  
+    getUserMetadata();
+  }, []);
 
   return (
     isAuthenticated && (
