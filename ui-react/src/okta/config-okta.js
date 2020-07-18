ff1d19e0f6c6d3b0b298d0b842956a2e0e4fe3a4
Landon Turner <lturner@teamworks.com>
2020-07-18 15:50:40 -0400

    finish okta configure

--- ui-react/src/okta/config-okta.js
+++ ui-react/src/okta/config-okta.js
@@ -1,10 +1,13 @@
 const CLIENT_ID = process.env.REACT_APP_OKTA_CLIENT_ID
 const OKTA_DOMAIN = process.env.REACT_APP_OKTA_DOMAIN
+const CALLBACK_PATH = '/implicit/callback';
+
+// need to figure out if I need to set the host dynamically
 
 export const configOkta = {
     clientId: `${CLIENT_ID}`,
     issuer: `https://${OKTA_DOMAIN}/oauth2/default`,
-    redirectUri: 'http://localhost:8080/implicit/callback',
+    redirectUri: `http://localhost:8080/${CALLBACK_PATH}`,
     scopes: ['openid', 'profile', 'email'],
     pkce: true
   };
