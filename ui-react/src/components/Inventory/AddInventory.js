daf8227ae1958176e0af55d22b3eeb6cac906c64
Landon Turner <lturner@teamworks.com>
2020-07-20 19:04:33 -0400

    elements on single item page

--- ui-react/src/components/Inventory/AddInventory.js
+++ ui-react/src/components/Inventory/AddInventory.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { addDept, addItem, getAllDepts } from '../../redux/actions/warehouseActions'
 import Button from '@material-ui/core/Button';
--- ui-react/src/components/Inventory/ItemPage.js
+++ ui-react/src/components/Inventory/ItemPage.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getItem } from '../../redux/actions/warehouseActions';
+import Button from '@material-ui/core/Button';
 
 const ItemPage = () => {
+    const { id } = useParams(); // params hook grabs the id of the item
+    const dispatch = useDispatch();
+    const item = useSelector(state => state.warehouseReducer.singleItem);
+    useEffect(()=> {
+        dispatch(getItem(id))
+    },[])
+
+    console.log(item)
+    console.log(item.isAvailable)
     return (
         <div>
-            <h2>Single Item Here</h2>
+            <h2>{item.itemName}</h2>
+            <img src={item.mainImgUrl} />
+            <p>{item.description}</p>
+            { item.isCustomizable ?
+                <div>Customizable</div>
+                :
+                null
+            }
+            <p>Rental Rate: ${item.rentalRate}</p>
+            <p>Purchase: ${item.buyNow}</p>
+            {/* How should I show the quantity */}
+            <p>{item.quantity || 0} units available</p>
+            { item.isAvailable ?
+                <div>
+                    <Button>Reserve to Rent</Button>
+                    <Button>Buy</Button>
+                </div>
+                :
+                <div>
+                    <Button disabled>Not Available</Button>
+                    <Button>Buy</Button>
+                </div>
+            }
         </div>
     )
 };
