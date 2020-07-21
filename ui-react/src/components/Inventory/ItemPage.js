0facdccef56d31466d19535b1c7392adc1c69ba5
Landon Turner <lturner@teamworks.com>
2020-07-20 20:46:44 -0400

    add superUser check to single item page

--- ui-react/src/components/Inventory/ItemPage.js
+++ ui-react/src/components/Inventory/ItemPage.js
@@ -1,20 +1,43 @@
-import React,{ useEffect } from 'react';
+import React,{ useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { useOktaAuth } from '@okta/okta-react';
 import { getItem } from '../../redux/actions/warehouseActions';
+
 import Button from '@material-ui/core/Button';
 import { noImg } from '../../assets/imageAssets';
 
+
 const ItemPage = () => {
     const { id } = useParams(); // params hook grabs the id of the item
+    const { authState, authService } = useOktaAuth();
+    const [userInfo, setUserInfo] = useState(null);
+    const [superUser, setSuperUser] = useState(false);// state for workaround
+    const whitelist = ["00ul53sdvnWjre0aF4x6"];
+    // workaround until I can pass superUser attribute from okta
+    const checkSuperUser = (user) => {
+        console.log("user in function", user)
+        let verdict = whitelist.includes(user)
+        return verdict ? setSuperUser(true) : null
+    };
     const dispatch = useDispatch();
     const item = useSelector(state => state.warehouseReducer.singleItem);
     useEffect(()=> {
+        if (!authState.isAuthenticated) {
+            // When user isn't authenticated, forget any user info
+            setUserInfo(null);
+            } else {
+            authService.getUser().then((info) => {
+                checkSuperUser(info.sub) // uses the sub from the healthy response for workaround to show item CRUD
+                setUserInfo(info);
+            });
+        }
         dispatch(getItem(id))
-    },[]);
+    },[authState, authService,checkSuperUser()]);
 
     console.log(item)
     console.log(item.isAvailable)
+    console.log("super user status", superUser)
     return (
         <div>
             <Button href="/inventory">Back</Button>
