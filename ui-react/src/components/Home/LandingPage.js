2380d27087abb48d0239280fe3a95800bbee37fe
Landon Turner <lturner@teamworks.com>
2020-07-26 11:54:42 -0400

    add landing page elements

--- ui-react/src/components/Home/LandingPage.js
+++ ui-react/src/components/Home/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllItems } from '../../redux/actions/warehouseActions'
+import { Button } from '@material-ui/core';
+import Spinner from '../material-ui/Spinner';
 
 // SHOULD THIS JUST BE AN INVENTORY PAGE?
 
@@ -11,8 +13,8 @@ const LandingPage = () => {
 
     const getFeaturedItem = () => {
         const featIndex = Math.floor(Math.random() * Math.floor(items.length));
-        // filter array by index then set the item
-        // setFeatured(filter) 
+        // find by index and set the item
+        setFeatured(items[featIndex]);
     };
     // get items from store
     useEffect(()=>{
@@ -23,19 +25,28 @@ const LandingPage = () => {
         getFeaturedItem()
     },[items])
 
-    console.log(items)
+    console.log(featured)
 
     return(
         <div>
             <h1>🚧 Landing Page 🚧</h1>
             <p>This is page is under construction. Please go <a href="/inventory">HERE</a> to check out the inventory</p>
             <div>
-                <img />
+                <img src="https://images.unsplash.com/photo-1521459444000-c2192d774976?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="wedding aisle" />
                 <p>Facilitate the perfect day</p>
             </div>
             <div className="featured">
                 <h3>Featured</h3>
-                <p>Featured Item here</p>
+                {/* Eventually will map through the array when there is enough inventory */}
+                { featured ?
+                    <div>
+                        <h4>{featured.itemName}</h4>
+                        <p>Rent: ${featured.rentalRate}</p>
+                        <Button href={`/inventory/item/${featured.id}`}>See More</Button>
+                    </div>   
+                    :
+                        <Spinner />   
+                }
             </div>
 
         </div>
