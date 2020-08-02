bf5361962405c019c799d9c87811543e68800e85
Landon Turner <lturner@teamworks.com>
2020-08-02 16:54:44 -0400

    calendar scaffolded

--- ui-react/src/components/Inventory/ReserveItem.js
+++ ui-react/src/components/Inventory/ReserveItem.js
@@ -4,13 +4,17 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getItem, deleteItem } from '../../redux/actions/warehouseActions';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const ReserveItem = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
     return(
         <div>
             <h2>Reserve Item</h2>
             <form>
                 <input placeholder="work in progress" />
+                <Calendar />
             </form>
         </div>
     )
