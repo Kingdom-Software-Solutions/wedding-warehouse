ee90e80d6212ebe713e594d81cf7564f445d906e
Landon Turner <lturner@teamworks.com>
2020-09-17 22:44:05 -0400

    initial commit, first pass at cart actions and reducer

---
+++ ui-react/src/components/Navigation/Cart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+const Cart = () => {
+    const history = useHistory();
+    
+    return(
+        <div>
+            Cart
+        </div>
+    )
+};
+
+export default Cart;
---
+++ ui-react/src/redux/reducers/cartReducer.js
@@ -0,0 +1,33 @@
+import {
+    ADD_ITEM,
+    REMOVE_ITEM,
+    CLEAR_CART
+} from '../actions/cartActions';
+
+import { initialState } from './cartInitialState';
+
+export const cartReducer = (state = initialState, action) => {
+    const { type, payload } = action;
+    switch(type){
+        case ADD_ITEM:
+            return {
+                ...state,
+                items: items.push(payload)
+            }
+        case REMOVE_ITEM:
+            // should be handled in action or no? Start with this
+            const updated = items.filter(item => item.id !== payload)
+            return {
+                ...state,
+                items: updated
+            }
+        case CLEAR_CART:
+            return{
+                ...state,
+                items: []
+            }
+        default:
+            return state
+    }
+}
+
