6288e6994b40e2184a9051bf542c8f26a52cda41
Landon Turner <lturner@teamworks.com>
2020-09-17 23:08:24 -0400

    added code to cart, last commit tonight

--- ui-react/src/components/Navigation/Cart.js
+++ ui-react/src/components/Navigation/Cart.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { removeFromCart, clearCart } from '../../redux/actions/cartActions';
 
 const Cart = props => {
     const history = useHistory();
     const dispatch = useDispatch();
     const cartItems = useSelector(state => state.cartReducer.items)
     console.log(cartItems)
+
+    // add event handlers to remove item and clear cart 
     
     const handleCloseCart = e => {
         props.setOpenCart(false)
@@ -16,11 +19,16 @@ const Cart = props => {
             <span onClick={handleCloseCart}>X</span>
             <h2>Your Cart</h2>
             {cartItems.map(item => {
+                <>
                 <div key={item.id}>
                     <p>{item.itemName}</p>
                     <p>{item.rental}</p>
                 </div>
+                <span>X</span>
+                </>
             })}
+            <button>Clear Items</button>
+            <button>Checkout</button>
         </div>
     )
 };
