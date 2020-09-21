6925463432e00b04b561b5f609fe54d3d364fffb
Landon Turner <lturner@teamworks.com>
2020-09-21 12:35:01 -0400

    add notes to checkout component

--- ui-react/src/components/Inventory/reservations/Checkout.js
+++ ui-react/src/components/Inventory/reservations/Checkout.js
@@ -2,3 +2,7 @@ import React from 'react';
 
 
 // build checkout page
+// useEffect to check the reservations on the items in the cart
+    // for each item in the cart, check if the reservations have a conflict in the date range selected
+    // have a the user set a date range
+    // pull that items reservations and check them, if there is one, grey out the item and remove it's rent per day from the total
