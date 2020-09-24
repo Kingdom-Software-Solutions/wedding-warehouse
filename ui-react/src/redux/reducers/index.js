d2aee5730c2aa12c0673bcf9a4826b9092dc3183
Landon Turner <lturner@teamworks.com>
2020-09-23 22:08:21 -0400

    add redux persist to maintain cart state

--- ui-react/src/redux/reducers/index.js
+++ ui-react/src/redux/reducers/index.js
@@ -1,12 +1,23 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { userReducer } from './userReducer';
 import { warehouseReducer } from './warehouseReducer';
 import { reserveReducer } from './reserveReducer';
-import { cartReducer } from './cartReducer'
+import { cartReducer } from './cartReducer';
 
-export const rootReducer = combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cartReducer'] //Add any new reducers to this array for perisitence to work
+}
+
+const rootReducer = combineReducers({
     userReducer,
     warehouseReducer,
     reserveReducer,
     cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
