6d757a9451e99cf844ad9036df26d73b1fc4f7b5
Landon Turner <lturner@teamworks.com>
2020-10-05 22:51:58 -0400

    updating last commit

--- ui-react/src/redux/actions/oktaActions.js
+++ ui-react/src/redux/actions/oktaActions.js
@@ -6,36 +6,7 @@ export const OKTA_UPDATE_SUCCESS = 'OKTA_UPDATE_SUCCESS';
 export const OKTA_UPDATE_FAILURE = 'OKTA_UPDATE_FAILURE';
 
 export const updateUserProfile = (updates) => dispatch => {
-    // example profile object:
-    ```
-    {
-        "profile": {
-          "login": "isaac.brock@example.com",
-          "firstName": "Isaac",
-          "lastName": "Brock",
-          "nickName": "issac",
-          "displayName": "Isaac Brock",
-          "email": "isaac.brock@example.com",
-          "secondEmail": "isaac@example.org",
-          "profileUrl": "http://www.example.com/profile",
-          "preferredLanguage": "en-US",
-          "userType": "Employee",
-          "organization": "Okta",
-          "title": "Director",
-          "division": "R&D",
-          "department": "Engineering",
-          "costCenter": "10",
-          "employeeNumber": "187",
-          "mobilePhone": "+1-555-415-1337",
-          "primaryPhone": "+1-555-514-1337",
-          "streetAddress": "301 Brannan St.",
-          "city": "San Francisco",
-          "state": "CA",
-          "zipCode": "94107",
-          "countryCode": "US"
-        }
-      }
-    ```
+
     dispatch({type: OKTA_UPDATE_START});
     // there are a few endpoints we could use, depends on if they need to crud credentials or not. At first I say no and will go with the current user endpoint
     axiosWithOkta().post("api/v1/users/me", updates)
