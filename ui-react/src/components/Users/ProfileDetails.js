6a51b5d38beddaac24222a81702f708d134aa82d
Landon Turner <lturner@teamworks.com>
2020-10-31 07:16:50 -0400

    update wording, add comments in backend

--- ui-react/src/components/Users/ProfileDetails.js
+++ ui-react/src/components/Users/ProfileDetails.js
@@ -14,8 +14,8 @@ const ProfileDetails = ({ userInfo }) => {
         <>
             {!showEdit ?
                 <div>
-                <p>Welcome, {userInfo.first_name}!</p>
-                <p>It's great to see you, see your upcoming, or past reservations by clicking "Menu". Take a look at what we have for your event <a href="/inventory">here</a>!
+                <p>{userInfo.first_name} {userInfo.last_name}</p>
+                <p>It's great to see you! See your upcoming or past reservations by clicking "Menu". Take a look at what we have for your event <a href="/inventory">here</a>!
                 </p>
                 {/* UNCOMMENT IF WORKING ON OKTA STRETCH */}
                 {/* <button onClick={()=> toggleEdit()}>Edit Profile</button> */}
