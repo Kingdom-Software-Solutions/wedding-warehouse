ae9460779f0d77bc9f3e98e2ed08184bed9d8204
Landon Turner <lturner@teamworks.com>
2020-11-07 06:57:45 -0500

    commit changes

--- routers/middleware/index.js
+++ routers/middleware/index.js
@@ -1,10 +1,9 @@
 const Models = require('../helpers/models')
 
-
-
-
 module.exports = {
-    validateUserId,
+    validateUserId, // depracated
+    validateDeptId,
+    validateDeptName
 }
 
 //custom middleware
@@ -22,6 +21,39 @@ function validateUserId(req, res, next) {
     .catch(err => {
       res.status(500).json({message: 'Could not get user'})
     });
+}
+
+function validateDeptId(req, res, next) {
+  const { id } = req.params;
+  Models.Department.findById(id)
+  .then(dept => {
+    if(dept){
+      req.dept = dept;
+      next();
+    } else {
+      res.status(404).json({message: `Department with the id of ${id} was not found`})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({message: 'Could not get Department'})
+  });
+}
+
+function validateDeptName(req, res, next) {
+  const deptObject = req.body;
+  const {name} = deptObject
+  Models.Department.findBy(deptObject)
+  .then(dept => {
+    if(dept.length > 0){
+      req.dept = dept;
+      next();
+    } else {
+      res.status(404).json({message: `Department with the name of ${name} was not found`})
+    }
+  })
+  .catch(err => {
+    res.status(500).json({message: 'Could not get this Department'})
+  });
 }
 
 
