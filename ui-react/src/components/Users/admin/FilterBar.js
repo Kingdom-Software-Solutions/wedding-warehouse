3d5e355dd19c66350a3748a62ef8d3f442f24362
Landon Turner <lturner@teamworks.com>
2020-11-22 16:08:19 -0500

    add chakra elements to filter bar

--- ui-react/src/components/Users/admin/FilterBar.js
+++ ui-react/src/components/Users/admin/FilterBar.js
@@ -1,16 +1,63 @@
 // file for admin table filters
-import React, {useState} from 'react';
-
-// Check chakra for snack bars!!
+import React, { useState, useEffect } from 'react';
+import { Grid, GridItem, Container, Input, Text, Select } from "@chakra-ui/react"
 
 
 const FilterBar = (reservations) => {
+    console.log(reservations)
+    const [values, setValues] = useState({
+        name: "",
+        startDate: "",
+        returnDate: "",
+        returnStatus: null
+    })
+
+    const handleChanges = e => {
+        e.preventDefault();
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return(
-        <div style={{display: 'flex', justifyContent: 'center'}}>
-        <span>Filter Name</span>
-        <span>Filter Dates</span>
-        <span>Filter Returned</span>
-        </div>
+        <>
+        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+            <GridItem style={{display: 'flex'}}>
+                <Text mb="8px">Filter Name</Text>
+                <Input          
+                value={values.name}
+                name="name"
+                onChange={handleChanges}
+                placeholder="Search a name"
+                size="sm"/>
+            </GridItem>
+            <GridItem style={{display: 'flex'}}>
+                <Text mb='8px'>Filter Dates:</Text>
+                <Input
+                    value={values.startDate}
+                    name="startDate"
+                    onChange={handleChanges}
+                    type='date'
+                    size="sm"
+                />
+                    <Input
+                    value={values.returnDate}
+                    name="returnDate"
+                    onChange={handleChanges}
+                    type='date'
+                    size="sm"
+                />
+            </GridItem>
+            <GridItem style={{display: 'flex'}}>
+                <Text mb='8px'>Filter Status</Text>
+                <Select name='returnStatus' placeholder="Select Status">
+                    <option value="true">Returned</option>
+                    <option value="false">Not Returned</option>
+                </Select>
+            </GridItem>
+        </Grid>
+        </>
     )
 }
 
